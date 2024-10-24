fe2a29cb34b3a8523225bae4f10e3f814529081b
TaiNgo30 <ngoductai2004@gmail.com>
2024-10-24 23:18:45 +0700

    Fix linter issues in test_app.py

--- starter/backend/test_app.py
+++ starter/backend/test_app.py
@@ -1,19 +1,16 @@
 from . import app
 import os
 
-
 def test_movies_endpoint_returns_200():
     with app.test_client() as client:
         status_code = os.getenv("FAIL_TEST", 200)
         response = client.get("/movies/")
         assert response.status_code == status_code
 
-
 def test_movies_endpoint_returns_json():
     with app.test_client() as client:
         response = client.get("/movies/")
         assert response.content_type == "application/json"
-
 
 def test_movies_endpoint_returns_valid_data():
     with app.test_client() as client:
